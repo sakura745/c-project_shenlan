f8a345de15f55bedfbf0ab3a1cdb25ecc7eddaae
CarBO <745879871@qq.com>
2021-10-16 00:49:30 +0800

    Oct 15

--- project1.cpp
+++ project1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <chrono>
+#include <iomanip>
 
 int main() {
     int Cnt = 0;
@@ -12,13 +13,15 @@ int main() {
     std::cout << "Type in what full marks you want: ";
     std::cin >> maxScore;
 
-    int i = 0;
-    std::cout << "Select add and type 1, subtract and type 2, multiply and type 3, divide and type 4: ";
-    std::cin >> i;
-
     int maxValue = 0;
     std::cout << "Please give the maximum range of calculation: ";
     std::cin >> maxValue;
+
+    std::string str;
+    std::cout << "Select mode esay and type \'easy\', hard and type \'hard\': ";
+    std::cin >> str;
+
+    std::cout << "Keep two decimal places!\n";
 
     int correctCnt = 0;
     int wrongCnt = 0;
@@ -27,34 +30,49 @@ int main() {
     srand(time(nullptr));//srand和rand不能放到循环里，否则每次生成的随机数对会不变。
 
     while (tmp) {
-        int a = rand() % maxValue + 1;
-        int b = rand() % maxValue + 1;
-        int res = 0;
+        double a = static_cast<double>(rand()) / static_cast<double>(RAND_MAX / maxValue);
+        double b = static_cast<double>(rand()) / static_cast<double>(RAND_MAX / maxValue);
+        double res = 0;
 
-        switch (i) {//四则运算的选择
-            case 1:
+        std::string s;
+        if (str == "easy") {
+            int p = rand() % 2 + 1;
+            if (p == 1) {
+                s = " + ";
                 res = a + b;
-                break;
-            case 2:
-                res = abs(a - b);
-                break;
-            case 3:
+            } else {
+                s = " - ";
+                res = a - b;
+            }
+        } else if (str == "hard") {
+            int p = rand() % 4 + 1;
+            if (p == 1) {
+                s = " + ";
+                res = a + b;
+            } else if (p == 2) {
+                s = " - ";
+                res = a - b;
+            } else if (p == 3) {
+                s = " * ";
                 res = a * b;
-                break;
-            case 4:
+            } else {
+                s = " / ";
                 res = a / b;
-                break;
-            default:
-                std::cout << "Please enter the correct option!!!\n";
+            }
+        } else {
+            std::cout << "Please enter the correct option!!!\n";
+            break;
         }
+
 
         if (res > maxValue)//确保是res以内的四则运算
             continue;
         tmp--;
 
-        std::cout << "\nGiven two numbers " << a << " and " << b << '\n';
+        std::cout << std::setprecision(2) << std::setiosflags(std::ios::fixed)
+                  << "\nGiven two numbers " << a << s << b << '\n';
         auto start = std::chrono::steady_clock::now();
-        int input = 0;
+        double input = 0;
         std::cout << "Please compute the result: ";
         std::cin >> input;
         auto end = std::chrono::steady_clock::now();
@@ -63,12 +81,12 @@ int main() {
         totalTime += std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
         minTime = std::min(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count(), minTime);
 
-        if (input == res) {
+        if ((double)std::abs(input - res) < 0.01) {//注意绝对值也有类型！！！
             correctCnt++;
             double sum = maxScore / Cnt;
             totalScore += sum;
         } else {
-            std::cout << "Em! The correct answer is: " << a << " and " << b << " is " << res << std::endl;
+            std::cout << "Em! The correct answer is: " << a << s << b << " = " << res << std::endl;
             wrongCnt++;
         }
     }
@@ -79,7 +97,7 @@ int main() {
     } else if (correctCnt == 0) {
         std::cout << "Come on~ Please be serious!! \n";
     }
-    std::cout << "Your total score is " << (int)totalScore << " and the full marks is " << maxScore << '\n'
+    std::cout << "Your total score is " << (int)totalScore << " and the full marks is " << (int)maxScore << '\n'
               << "MinTime is " << minTime << "ms, " << "Average Time is " << totalTime / Cnt << "ms\n"
               << "There are " << Cnt << " questions\n"
               << "The correct number of question is " << correctCnt << '\n'
